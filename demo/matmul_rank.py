7539c01499cf4810f709ebdf96bbcd4acd412383
Fedor Kuyanov <feodor.kuyanov@gmail.com>
2026-04-12 20:26:42 +0000

    Added matmul script

---
+++ demo/matmul_rank.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.path.append('..')
+from FedroTensor.tensor_rank import cp_rank_factorise
+from FedroTensor.matmul_tensor import build_matmul_tensor
+
+if len(sys.argv) != 5:
+    print(f'usage: {sys.argv[0]} n m p r', file=sys.stderr)
+    exit(1)
+n, m, p, r = map(int, sys.argv[1:5])
+T = build_matmul_tensor(n, m, p)
+factors = cp_rank_factorise(T, r, num_attempts=10**9, verbose=True)
+if factors is not None:
+    print(f'SUCCESS n={n} m={m} p={p} r={r}')
+else:
+    print(f'FAIL n={n} m={m} p={p} r={r}')
